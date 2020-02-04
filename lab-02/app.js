b52d0b8513414b5147ce23c182b447c63818fccc
will-ing <willkoger@gmail.com>
2020-02-04 12:52:29 -0800

    Adding HTML, css, and js

---
+++ lab-02/app.js
@@ -0,0 +1,17 @@
+// first question prompting yes or no
+var myFavColor = prompt('Is my favorite color blue?');
+
+if (myFavColor.toLowerCase().trim() === 'yes' || 'true') {
+ // console.log('That was a lucky guess');
+  alert('That was a lucky guess');
+} else {
+  // console.log('Well now you only missed one');
+  alert('Well now you only missed one');
+}
+
+// 2nd question 
+var myCommute = prompt('I drive my truck to school, correct?');
+
+if (myCommute.toLowerCase().trim(). === 'yes' || 'true')
+ // console.log('Nope I take the train less gas and less traffic.');
+  alert('Nope I take the train less gas and less traffic.');
