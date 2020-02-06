97f82572b66bbe2ac532482e16c7915992ea1a62
will-ing <willkoger@gmail.com>
2020-02-05 18:19:49 -0800

    add comments

--- lab-02/app.js
+++ lab-02/app.js
@@ -1,17 +1,17 @@
 'use strict'
 
-var yName = prompt('what is your name?');
+var yName = prompt('what is your name?'); // enter name
 
-var correctAns = [];
+var correctAns = []; // arr for ttl crt ans
 
 function name() {
   
-  var conf = confirm('Welcome ' + yName + ' do you want to play a game?');
+  var conf = confirm('Welcome ' + yName + ' do you want to play a game?'); // ret true to init game.
 
   if (conf === true) {
-    mColor();
-  } else {
-    alert('Don\'t be a square!')
+    mColor(); // prompts next question
+  } else { // exits out --> runs thankyou func
+    alert('Don\'t be a square!') 
   }
 }
 
@@ -23,12 +23,12 @@ function mColor() {
   if (myFavColor.toLowerCase().trim() === 'yes' || myFavColor.toLowerCase().trim() === 'true') {
   // console.log('That was a lucky guess');
     alert('That was a lucky guess');
-    correctAns.push(1);
-    mCommute();
+    correctAns.push(1); // add to arr ttl crt
+    mCommute(); // prompts next question
   } else  {
     // console.log('Well now you only missed one');
     alert('Well now you only missed one');
-    mCommute();
+    mCommute(); // prompts next question
   }
 }
 
@@ -39,12 +39,12 @@ function mCommute() {
   if (myCommute.toLowerCase().trim() === 'yes' || myCommute.toLowerCase().trim() === 'true') {
   // console.log('Nope I take the train less gas and less traffic.');
     alert('Nope I take the train less gas and less traffic.');
-    correctAns.push(1);
-    mFavTeam();
+    correctAns.push(1); // add to arr ttl crt
+    mFavTeam(); // prompts next question
   } else {
     // console.log('Look at the brains on ' + name + '!')
     alert('Look at the brains on ' + yName + '!');
-    mFavTeam();
+    mFavTeam(); // prompts next question
   }
 }
 
@@ -55,12 +55,12 @@ function mFavTeam() {
   if (myFavTeam.toLowerCase().trim() === 'yes' || myFavTeam.toLowerCase().trim() === 'true') {
   //  console.log('building momentum!');
     alert('building momentum!');
-    correctAns.push(1);
-    mJump();
+    correctAns.push(1); // add to arr ttl crt
+    mJump(); // prompts next question
   } else {
   //  console.log('Keep practicing!');
     alert('Keep practicing!')
-    mJump();
+    mJump(); // prompts next question
   }
 }
 
@@ -71,12 +71,12 @@ function mJump() {
   if (myJump.toLowerCase().trim() === 'yes' || myJump.toLowerCase().trim() === 'true') {
   //  console.log('Multiple times, ' + yName + '!');
     alert('Multiple times, ' + yName + '!');
-    correctAns.push(1);
-    mHouse();
+    correctAns.push(1); // add to arr ttl crt
+    mHouse(); // prompts next question
   } else {
   //  console.log('Negative ghost rider!');
     alert('Negative ghost rider!')
-    mHouse();
+    mHouse(); // prompts next question
   }
 }
 
@@ -87,27 +87,27 @@ function mHouse() {
   if (myHouse.toLowerCase().trim() === 'yes' || myHouse.toLowerCase().trim() === 'true') {
   //  console.log('Good guess, it is a lot of work!');
     alert('Good guess, it is a lot of work!');
-    correctAns.push(1);
+    correctAns.push(1); // add to arr ttl crt
   } else {
   //  console.log('Maybe this isn\'t your kind of game');
     alert('Maybe this isn\'t your kind of game')
     
   }
-  mNum()
+  mNum() // prompts next question
 }
 
 
 
 function mNum() {
   // queston 6
-  for(var i = 4; i > 0; i--) {
-    var myFavNum = prompt('I\'ll give you ' + i + ' chances to guess my favorite number.');
+  for(var i = 4; i > 0; i--) { // reduces after every iterations
+    var myFavNum = prompt('I\'ll give you ' + i + ' chances to guess my favorite number.'); // prompts you and shows remaining guesses
   
-      if (myFavNum === '8') {
+      if (myFavNum === '8') { // crt ans to break loop
         //  console.log('Good guess');
           alert('Good guess!');
-          correctAns.push(1);
-          break;
+          correctAns.push(1); // add to arr ttl crt
+          break; // breaks loop and cont
         } else if (myFavNum < 8) {
         // console.log('Think bigger')  
           alert('think bigger')
@@ -120,34 +120,34 @@ function mNum() {
           
         }
    } 
-   mVehicles()     
+   mVehicles() // prompts next question
 }
 
 function mVehicles() {
 // question 7
-  var myCars = ['chevrolet', 'bmw', 'ford', 'dodge'];
-
+  var myCars = ['chevrolet', 'bmw', 'ford', 'dodge']; // my arr
+// this will run 6 time counting backwards
   for(var i = 6; i > 0; i--) {
-    var myVehicles = prompt('Name a car I have owned! You have ' + i + ' tries!');
+    var myVehicles = prompt('Name a car I have owned! You have ' + i + ' tries!'); /// This will give a prompt with the remaining chances
 
-    if (myVehicles === myCars[0] || myVehicles === myCars[1] || myVehicles === myCars[2] || myVehicles === myCars[3]) {
+    if (myVehicles === myCars[0] || myVehicles === myCars[1] || myVehicles === myCars[2] || myVehicles === myCars[3]) { // eval each sect of arr
       alert('nice');
-      correctAns.push(1);
-      break;
+      correctAns.push(1); // this will add to the arr that has our ttl crt 
+      break; // breaks loop and cont
     } else {
       alert('try again!');
     }
   }
-  alert('These are the vehicles I have owned ' + myCars[0] + ' ' + myCars[1] + ' ' + myCars[2] + ' ' + myCars[3] + '.' );
+  alert('These are the vehicles I have owned ' + myCars[0] + ' ' + myCars[1] + ' ' + myCars[2] + ' ' + myCars[3] + '.' ); // displays the crt ans
   }
   
   
 
 
 function thankYou() {
-  alert('Thanks for playing and getting to know me ' + yName + '! You got ' + correctAns.length + ' correct');
+  alert('Thanks for playing and getting to know me ' + yName + '! You got ' + correctAns.length + ' correct'); // counts arr and gives crt ans ttl
 }
-name()
+name() // this starts the game
 //mNum()
 //mVehicles()
-thankYou()
+thankYou() // lets you know its over and gives ttl crt
