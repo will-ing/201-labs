347a65b5e2b9c5cbc2c790ac847a674c39c7dbf7
will-ing <willkoger@gmail.com>
2020-02-07 08:18:38 -0800

    code is dry and better

--- lab-02/app.js
+++ lab-02/app.js
@@ -4,102 +4,45 @@ var yName = prompt('what is your name?'); // enter name
 
 var correctAns = []; // arr for ttl crt ans
 
+name() // this starts the game
+
+thankYou() // lets you know its over and gives ttl crt
+
 function name() {
   
   var conf = confirm('Welcome ' + yName + ' do you want to play a game?'); // ret true to init game.
 
   if (conf === true) {
-    mColor(); // prompts next question
+    combine('Is my favorite color blue?', 'That was a lucky guess', 'Well now you only missed one'); // first question
+
+    combine('I drive my truck to school, correct?', 'Nope I take the train less gas and less traffic.', 'Look at the brains on ' + yName + '!'); // second question
+
+    combine('Alright ' + yName + ', you think I like the mariners?', 'building momentum!', 'Keep practicing!'); // third question
+
+    combine('I\'ve jumped out of an airplane before, true or false?', 'Multiple times, ' + yName + '!', 'Negative ghost rider!'); // fourth question
+
+    combine('Do I own a home, yes or no?', 'Good guess, it is a lot of work!', 'Maybe this isn\'t your kind of game'); // fifth question
+
+    mNum(); // this runs question 6 & 7 Loops are unique
+
   } else { // exits out --> runs thankyou func
     alert('Don\'t be a square!') 
   }
 }
 
-
-function mColor() {
-  // first question prompting yes or no
-  var myFavColor = prompt('Is my favorite color blue?');
-
-  if (myFavColor.toLowerCase().trim() === 'yes' || myFavColor.toLowerCase().trim() === 'true') {
-  // console.log('That was a lucky guess');
-    alert('That was a lucky guess');
-    correctAns.push(1); // add to arr ttl crt
-    mCommute(); // prompts next question
-  } else  {
-    // console.log('Well now you only missed one');
-    alert('Well now you only missed one');
-    mCommute(); // prompts next question
-  }
-}
-
-function mCommute() {
-  // 2nd question 
-  var myCommute = prompt('I drive my truck to school, correct?');
-
-  if (myCommute.toLowerCase().trim() === 'yes' || myCommute.toLowerCase().trim() === 'true') {
-  // console.log('Nope I take the train less gas and less traffic.');
-    alert('Nope I take the train less gas and less traffic.');
-    correctAns.push(1); // add to arr ttl crt
-    mFavTeam(); // prompts next question
-  } else {
-    // console.log('Look at the brains on ' + name + '!')
-    alert('Look at the brains on ' + yName + '!');
-    mFavTeam(); // prompts next question
-  }
-}
-
-function mFavTeam() {
-  // 3rd question
-  var myFavTeam = prompt('Alright ' + yName + ', you think I like the mariners?');
-
-  if (myFavTeam.toLowerCase().trim() === 'yes' || myFavTeam.toLowerCase().trim() === 'true') {
-  //  console.log('building momentum!');
-    alert('building momentum!');
-    correctAns.push(1); // add to arr ttl crt
-    mJump(); // prompts next question
-  } else {
-  //  console.log('Keep practicing!');
-    alert('Keep practicing!')
-    mJump(); // prompts next question
-  }
-}
-
-function mJump() {
-  // 4th question
-  var myJump = prompt('I\'ve jumped out of an airplane before, true or false?');
-
-  if (myJump.toLowerCase().trim() === 'yes' || myJump.toLowerCase().trim() === 'true') {
-  //  console.log('Multiple times, ' + yName + '!');
-    alert('Multiple times, ' + yName + '!');
-    correctAns.push(1); // add to arr ttl crt
-    mHouse(); // prompts next question
-  } else {
-  //  console.log('Negative ghost rider!');
-    alert('Negative ghost rider!')
-    mHouse(); // prompts next question
-  }
-}
-
-function mHouse() {
-  // 5th question
-  var myHouse = prompt('Do I own a home, yes or no?');
-
-  if (myHouse.toLowerCase().trim() === 'yes' || myHouse.toLowerCase().trim() === 'true') {
-  //  console.log('Good guess, it is a lot of work!');
-    alert('Good guess, it is a lot of work!');
+function combine(q, rone, rtwo) { // uses 3 parameters 
+  var que = prompt(q); // prompts question and stores answer
+  
+  if  (que.toLowerCase().trim() === 'yes' || que.toLowerCase().trim() === 'true') { // evaluates if ans is true || false
+    alert(rone); // gives alert arg for crt ans
     correctAns.push(1); // add to arr ttl crt
   } else {
-  //  console.log('Maybe this isn\'t your kind of game');
-    alert('Maybe this isn\'t your kind of game')
-    
+    alert(rtwo); // gives alert arg for wrg ans
   }
-  mNum() // prompts next question
 }
-
-
 
 function mNum() {
-  // queston 6
+  // question 6
   for(var i = 4; i > 0; i--) { // reduces after every iterations
     var myFavNum = prompt('I\'ll give you ' + i + ' chances to guess my favorite number.'); // prompts you and shows remaining guesses
   
@@ -140,16 +83,7 @@ function mVehicles() {
     }
   alert('These are the vehicles I have owned ' + myCars[0] + ' ' + myCars[1] + ' ' + myCars[2] + ' ' + myCars[3] + '.' ); // displays the crt ans
   }
-  
-  
-
 
 function thankYou() {
   alert('Thanks for playing and getting to know me ' + yName + '! You got ' + correctAns.length + ' correct'); // counts arr and gives crt ans ttl
 }
-
-
-name() // this starts the game
-//mNum()
-//mVehicles()
-thankYou() // lets you know its over and gives ttl crt
